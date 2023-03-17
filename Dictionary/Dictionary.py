aedfb43c1c182c556cb20ba7b5a4c9ed3964bdf3
Einzel <einzel0102@mail.ru>
2023-03-17 21:18:30 +0800

    Словари

---
+++ Dictionary/Dictionary.py
@@ -0,0 +1,36 @@
+# Словари — неупорядоченные коллекции произвольных объектов с доступом по ключу.
+# В списках в качестве ключа используется индекс элемента. В словаре для
+# определения элемента используется значение ключа (строка, число).
+
+
+dictionary = {}
+dictionary ={'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+print(dictionary['left']) # ←
+# типы ключей могут отличаться
+print(dictionary['up']) # ↑
+# типы ключей могут отличаться
+dictionary['left'] = '⇐'
+print(dictionary['left']) # ⇐
+# print(dictionary['type']) # KeyError: 'type'
+# del dictionary['left'] # удаление элемента
+# for item in dictionary: # for (k,v) in dictionary.items():
+#     print('{}: {}'.format(item, dictionary[item]))
+# up: ↑
+# down: ↓
+# right: →
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
